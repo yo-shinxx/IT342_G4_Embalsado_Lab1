0499a7dbcac5ce8c4177c57931c58d95b4775718
yo-shinxx <shinelymarie.embalsado@cit.edu>
2026-02-20 17:32:39 +0800

    commented out duplicated cors statement

--- backend/src/main/java/com/quantix/backend/controller/AuthController.java
+++ backend/src/main/java/com/quantix/backend/controller/AuthController.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+// @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @AllArgsConstructor
 public class AuthController {
 
